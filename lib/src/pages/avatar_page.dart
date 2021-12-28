1ee3a4814e3137833cd73b3fed6fa36f6fd1a9c3
eduardo <eduardo.hdzdelarosa@gmail.com>
2021-12-28 09:54:41 -0700

    se agrgo un avatar circular en la pagina de avatars

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://duelbrand.files.wordpress.com/2013/06/logo-superman.png'),
+              radius: 20.0,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.green[300],
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://sm.ign.com/t/ign_latam/screenshot/default/superman-huesos-rotos_3fr8.1280.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
