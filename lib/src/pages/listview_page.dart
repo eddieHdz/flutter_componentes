62f9332c6c874f0785e3bc2709b03049177573a6
eduardo <eduardo.hdzdelarosa@gmail.com>
2021-12-29 13:12:30 -0700

    se creo un scroll infinito y un loading al cargar mas imagenes

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class listaPage extends StatefulWidget {
   @override
@@ -6,26 +7,92 @@ class listaPage extends StatefulWidget {
 }
 
 class _listaPageState extends State<listaPage> {
-  List<int> _listaNumeros = [1, 2, 3, 4, 5];
+  ScrollController _scrollController = new ScrollController();
+  List<int> _listaNumeros = [];
+  int _ultimoItem = 0;
+  bool _isLoading = false;
+  @override
+  void initState() {
+    super.initState();
+    _agregar();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        //_agregar();
+        _fetchData();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Listas'),
-      ),
-      body: _crearLista(),
-    );
+        appBar: AppBar(
+          title: Text('Listas'),
+        ),
+        body: Stack(
+          children: <Widget>[_crearLista(), _crearLoading()],
+        ));
   }
 
   Widget _crearLista() {
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _listaNumeros.length,
       itemBuilder: (BuildContext context, int index) {
         return FadeInImage(
-          image: NetworkImage('https://picsum.photos/seed/picsum/400/300'),
+          image: NetworkImage('https://picsum.photos/id/${index}/400/300'),
           placeholder: AssetImage('assets/jar-loading.gif'),
         );
       },
     );
+  }
+
+  void _agregar() {
+    for (var i = 1; i < 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+    setState(() {});
+  }
+
+  Future _fetchData() async {
+    _isLoading = true;
+    setState(() {});
+    final duration = new Duration(seconds: 2);
+    new Timer(duration, respuestaHTTP);
+  }
+
+  void respuestaHTTP() {
+    _isLoading = false;
+    _scrollController.animateTo(_scrollController.position.pixels + 100,
+        curve: Curves.fastOutSlowIn, duration: Duration(milliseconds: 250));
+    _agregar();
+  }
+
+  Widget _crearLoading() {
+    if (_isLoading) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [CircularProgressIndicator()],
+          ),
+          SizedBox(
+            height: 15.0,
+          )
+        ],
+      );
+    } else {
+      return Container();
+    }
   }
 }
